19d2b11ba398d93b2ecbf8785b4f5b05a6d3c0dc
Saif Alhail <sa22431@essex.ac.uk>
2023-01-19 14:51:04 +0300

    final

--- portfolio/views.py
+++ portfolio/views.py
@@ -10,14 +10,11 @@ from .price_prediction import *
 from .scraper import run_scraper
 from .ai_dataset_prep_df import run_ai
 from .nn_final import run_neural
-from django.views.decorators.csrf import csrf_exempt
-from ratelimit.decorators import ratelimit
 
 def home(request):
     return render(request, 'home.html', {})
 
-@ratelimit(key='ip', rate='1/m')
-@csrf_exempt 
+
 def lstm_algorithm(request):
     
     # Get the all files into pickle folder where _history is append at the end to show in the frontend filter for chart
